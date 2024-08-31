27fc4e8dbaf958a271603e3ba58b7ed5e0441997
Areeb Ahmed <135235925+areebahmeddd@users.noreply.github.com>
2024-09-01 03:17:05 +0530

    🍕 [v1.0.0] Folder Restructure

--- flutter-app/android/app/src/main/kotlin/com/example/mivro/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mivro
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- flutter-app/lib/models/message.dart
+++
@@ -1,6 +0,0 @@
-class Message {
-  final String text;
-  final bool isUser;
-
-  const Message({required this.text, required this.isUser});
-}
